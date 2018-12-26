b843bd6ac770495a3ec15f46a517c4e5bae41350
Jirapat Limpanich <jirapat.lpn@gmail.com>
2018-12-26 11:32:48 +0700

    first commit3

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1633002538');
+define('LINE_MESSAGE_CHANNEL_SECRET','578c8767e4bd330c0d10ac04bbe6927a');
+define('LINE_MESSAGE_ACCESS_TOKEN','AbtH5ksfJK9V+Y4yJd7ry+LazL+/R9CeC9E2krZuulNSwgjmznbqa7JvjogzMaNMVx4Nv+YElMixcFXcQM8cP2U71I20bMrFtoEDcDSQM0eMyKH2bFfzIBVdBFkMNSfsoQs6z5mI2DMKAH0f1O4sowdB04t89/1O/w1cDnyilFU=');
+?>
